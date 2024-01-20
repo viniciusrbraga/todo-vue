6462d7eb2cedfd69caab9042c86aa287c2092361
Vinicius Braga <viniciusrb@gmail.com>
2024-01-20 00:34:11 -0300

    Componentizando a aplicação

---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps(['tarefasFilts'])
+</script>
+<!-- getTarefasFiltradas() -->
+<!-- evento.target.checked -->
+<template>
+    <ul class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in props.tarefasFilts">
+            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+                :id="tarefa.titulo" type="checkbox">
+            <!-- ms-3 = margin start 3 -->
+            <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
+                {{ tarefa.titulo }}
+            </label>
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+.done {
+    text-decoration: line-through;
+}
+</style>
